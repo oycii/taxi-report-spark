64e9c2adf542ed716f12866332d2fd8ba7f3242a
oycii <a.v.logvinov@gmail.com>
2022-12-04 18:04:37 +0300

    Remove show input data

--- src/main/scala/com/github/oycii/taxi/report/Main.scala
+++ src/main/scala/com/github/oycii/taxi/report/Main.scala
@@ -18,13 +18,8 @@ object Main extends LazyLogging {
     SparkUtils.withSparkSession("taxi-report-spark", implicit spark => {
       val dataService = new DataService
       val dfTaxiRide = dataService.getData(DATA_TAXI_RAID, parquet).persist(StorageLevel.MEMORY_AND_DISK)
-      println("dfTaxiRide: " + dfTaxiRide)
-      dfTaxiRide.printSchema()
-      dfTaxiRide.show()
+
       val dfTaxiZone = dataService.getData(DATA_TAXI_ZONE, csv).persist(StorageLevel.MEMORY_AND_DISK)
-      println("dfTaxiZone: " + dfTaxiRide)
-      dfTaxiZone.printSchema()
-      dfTaxiZone.show()
 
       val dfTaxiPopDistrict = TaxiReportService.popDistricts(dfTaxiRide, dfTaxiZone)
       dataService.saveData(dfTaxiPopDistrict, DATA_TAXI_POP_DISTRICTS)
@@ -35,7 +30,7 @@ object Main extends LazyLogging {
       dataService.saveData(reportTaxiTime, DATA_TAXI_TIME)
 
       val dfTaxiDistances = TaxiReportService.popDistances(dfTaxiRide)
-      //dataService.saveData(dfTaxiDistances, DB_URL, DB_TABLE_NAME, DB_PROPERTIES)
+      dataService.saveData(dfTaxiDistances, DB_URL, DB_TABLE_NAME, DB_PROPERTIES)
 
     })
     logger.info("Application finished")
